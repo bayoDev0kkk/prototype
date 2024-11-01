4c8af1de749fd5ac75f6bccb8247631192f15078
Baiel Kanybekov <kanybekov245@gmail.com>
2024-11-01 19:53:37 +0600

    wrote the code and fix the bugs

--- core-proto-vehicle/train-or-truck.js
+++ core-proto-vehicle/train-or-truck.js
@@ -1,35 +1,39 @@
 // Функции-конструкторы Train и Truck имеют схожие свойства и поведение.
 // Перепишите их так, чтобы избавиться от дублирования одинаковых свойств и методов.
 function Vehicle(driver) {
-  // Код тут.
+  this.driver = driver;
 }
+
+Vehicle.prototype.drive = function (kmh) {
+  console.log(`${this.driver} is driving at ${kmh} km/h.`);
+};
+
+Vehicle.prototype.stop = function () {
+  console.log(`${this.driver} has stopped.`);
+};
+
 function Train(driver) {
-  // Код тут.
-  this.drive = function (kmh) {
-    // Код тут.
-  };
-  this.stop = function () {
-    // Код тут.
-  };
+  Vehicle.call(this, driver);
 }
+
+Train.prototype = Object.create(Vehicle.prototype);
+Train.prototype.constructor = Train;
 
 function Truck(driver) {
-  // Код тут.
-  this.drive = function (kmh) {
-    // Код тут.
-  };
-  this.stop = function () {
-    // Код тут.
-  };
-  this.loadCargo = function (cargo) {
-    // Код тут.
-  };
+  Vehicle.call(this, driver);
 }
 
-Truck.prototype.unloadCargo = function () {
-  // Код тут.
+Truck.prototype = Object.create(Vehicle.prototype);
+Truck.prototype.constructor = Truck;
+
+Truck.prototype.loadCargo = function (cargo) {
+  console.log(`${this.driver} is loading cargo: ${cargo}.`);
 };
-// экспорт Vehicle, Train, Truc в файл с тестами
+
+Truck.prototype.unloadCargo = function () {
+  console.log(`${this.driver} has unloaded the cargo.`);
+};
+
 module.exports = { Vehicle, Train, Truck };
 
 // Функции-конструкторы Train и Truck имеют схожие свойства и поведение.
